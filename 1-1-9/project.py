de1aeca66c1ac6f9daf2b814798d6521bde3a019
Jeremy <jeremy.garcia@vtsd.com>
2024-10-17 13:12:11 -0400

    1.1.8

---
+++ 1-1-9/project.py
@@ -0,0 +1,56 @@
+import turtle as trtl
+
+# sky code here (makes background with just a really big circle)
+# Finished
+sky = trtl.Turtle()
+sky.speed(0)
+sky.color('#00a6ff')
+sky.pensize(5000)
+sky.circle(100)
+sky.hideturtle()
+
+# grass/ground code
+#Finished
+grass = trtl.Turtle()
+grass.speed(0)
+grass.color('#3f9b0b')
+grass.pensize(250)
+grass.penup()
+grass.goto(-500, -350)
+grass.pendown()
+grass.forward(1000)
+grass.hideturtle()
+
+#cloud code
+cloud = trtl.Turtle()
+cloud.color('white')
+cloud.pensize(50)
+cloud.penup()
+cloud.goto(400, 300)
+cloud.pendown()
+while
+cloud.circle(5)
+
+
+
+# house code
+house = trtl.Turtle()
+
+# sun code
+sun = trtl.Turtle()
+
+
+
+
+
+
+
+
+
+
+
+
+
+# the loop stuff to keep it there
+wn = trtl.Screen()
+wn.mainloop()
